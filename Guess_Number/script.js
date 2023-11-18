f239a2d3fef4eb0f1bf2a607c4abb529f2d62445
sarb <sarbjeetb2@gmail.com>
2023-11-18 13:04:04 -0700

    update with refectoring

--- Guess_Number/script.js
+++ Guess_Number/script.js
@@ -5,16 +5,19 @@ let secretNumber=Math.trunc(Math.random()*20+1);
 let score=20;
 let highscore=0;
 
+const displayMessage =(massage)=>    document.querySelector('.message').textContent
+=massage;
+
 
 document.querySelector('.check').addEventListener('click',function(){
    const guess= Number(document.querySelector('.guess').value);
    // no input
    if(!guess){
-    document.querySelector('.message').textContent= '🥹 No Number';
+    displayMessage('🙌🏻 No Number');
    
    } // winning option
    else if(guess===secretNumber){
-    document.querySelector('.message').textContent= '🥳 Correct Number!';
+    displayMessage('🥳 Correct Number!');
     document.querySelector('.number').textContent=secretNumber;
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width= '30rem';
@@ -29,7 +32,7 @@ document.querySelector('.check').addEventListener('click',function(){
    } else if(guess!==secretNumber){
  
         if(score>1){
-        document.querySelector('.message').textContent= guess>secretNumber?'🙌🏻 Too High !':'🙌🏻 Too Low';
+        displayMessage(guess>secretNumber?'🙌🏻 Too High !':'🙌🏻 Too Low');
         score--;
         document.querySelector('.score').textContent=
         score;
@@ -37,7 +40,7 @@ document.querySelector('.check').addEventListener('click',function(){
         =score;
         
         }else{
-            document.querySelector('.message').textContent= '🙌🏻 You Lost The Game';
+            displayMessage('🙌🏻 You Lost The Game');
             document.querySelector('.score').textContent=0;
         }
       
@@ -50,7 +53,7 @@ document.querySelector('.again').addEventListener('click',function(){
 
     score=20;
     let guess= Number(document.querySelector('.guess').value);
-    document.querySelector('.message').textContent="Start Guess !";
+    displayMessage("Start Guess !");
     document.querySelector('.score').textContent=score;
     document.querySelector('.number').textContent='?';
     document.querySelector('.guess').value='';
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Javascript_Learning
+Java Script Guess a number game . DOM methods and properties are web API are written in JAVASCRIPT .i used selector and addEventListener . 
