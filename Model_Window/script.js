9b5b4b8a7f2792e3d55fbd35c1533911afa6a47e
sarb <sarbjeetb2@gmail.com>
2023-11-18 14:33:24 -0700

    modal close on press  escape

--- Model_Window/script.js
+++ Model_Window/script.js
@@ -20,4 +20,13 @@ const closeModal= function(){modal.classList.add('hidden');
 btnsCloseModel.addEventListener('click',closeModal);
 overlay.addEventListener('click',closeModal);
 
+document.addEventListener('keydown',function(e){
+    if(e.key ==='Escape'){
+        if(!modal.classList.contains('hidden')){
+            closeModal();
+        }
+        
+    }
+});
+
 
